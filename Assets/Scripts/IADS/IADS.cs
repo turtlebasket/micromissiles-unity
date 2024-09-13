d0ef6c3fc4e24b82700a1e8ee91ca49f2d894711
Daniel Lovell <dlovell98@eecs.berkeley.edu>
2024-09-12 20:06:47 -0700

    WIP IADS and Vessel structure

---
+++ Assets/Scripts/IADS/IADS.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+
+// Integrated Air Defense System
+public class IADS : MonoBehaviour
+{
+
+    public enum TargetStatus {
+        UNASSIGNED,
+        ASSIGNED,
+        HIT,
+        DEGRADED,
+        DESTROYED
+    }
+
+    // Look up target status by unique target ID
+    public Dictionary<string, TargetStatus> _targetStatusDictionary;
+    
+    private List<Target> _targets;
+
+    private List<Missile> _missiles;
+
+    private List<Vessel> _vessels;
+
+    public delegate void RegisterNewTargetDelegate(Target target);
+    public event RegisterNewTargetDelegate OnRegisterNewTarget;
+
+    void Start()
+    {
+        _targets = new List<Target>();
+    }
+
+    public void RegisterNewTarget(Target target) {
+        _targets.Add(target);
+        OnRegisterNewTarget?.Invoke(target);
+    }   
+}
---
+++ Assets/Scripts/IADS/Vessel.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Vessel : MonoBehaviour
+{
+    [SerializeField]
+    private List<Missile> missileInventory = new List<Missile>();
+
+    public void AddMissile(Missile missile)
+    {
+        if (missile != null)
+        {
+            missileInventory.Add(missile);
+        }
+    }
+
+    public void RemoveMissile(Missile missile)
+    {
+        missileInventory.Remove(missile);
+    }
+
+    public List<Missile> GetMissileInventory()
+    {
+        return new List<Missile>(missileInventory);
+    }
+
+    public int GetMissileCount()
+    {
+        return missileInventory.Count;
+    }
+
+    // Additional methods can be added here as needed
+}
