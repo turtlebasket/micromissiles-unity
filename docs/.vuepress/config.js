4fb40a2bc624e0e6dc5117553f4542d561caafa2
Daniel Lovell <dlovell98@eecs.berkeley.edu>
2024-09-30 02:26:02 -0700

    Attempt to fix vuepress site

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,32 @@
-module.exports = {
-  title : 'Micromissiles Unity Project',
-  description : 'Documentation for the Micromissiles Unity project',
-  base : '/micromissiles-unity/',
-  themeConfig : {
-    navbar :
-           [
-             {text : 'Home', link : '/'}, {
-               text : 'Keybinds and Controls',
-               link : '/Keybinds_and_Controls.html'
-             },
-             {
-               text : 'Simulation Configuration Guide',
-               link : '/Simulation_Config_Guide.html'
-             },
-             {text : 'Simulation Logging', link : '/Simulation_Logging.html'}, {
-               text : 'Coverage Reports',
-               link : '/coverage/'
-             },  // Link to coverage reports
-           ],
-    sidebar : 'auto',
-  },
-  // Add this to ensure INDEX.md is used as the home page
-  patterns : ['INDEX.md', '**/*.md', '**/*.vue'],
+module.exports = async () => {
+  const { viteBundler } = await import('@vuepress/bundler-vite')
+  
+  return {
+    title : 'Micromissiles Unity Project',
+    description : 'Documentation for the Micromissiles Unity project',
+    base : './',
+    themeConfig : {
+      navbar :
+             [
+               {text : 'Home', link : '/'}, {
+                 text : 'Keybinds and Controls',
+                 link : '/Keybinds_and_Controls.html'
+               },
+               {
+                 text : 'Simulation Configuration Guide',
+                 link : '/Simulation_Config_Guide.html'
+               },
+               {text : 'Simulation Logging', link : '/Simulation_Logging.html'}, {
+                 text : 'Coverage Reports',
+                 link : '/coverage/'
+               },  // Link to coverage reports
+             ],
+      sidebar : 'auto',
+    },
+    bundler: viteBundler(),
+    public: '../images',
+    // Add this to ensure INDEX.md is used as the home page
+    patterns : ['INDEX.md', '**/*.md', '**/*.vue'],
+  }
 }
+
